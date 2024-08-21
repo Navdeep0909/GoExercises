a39508d99f35d66b7f36b3c6e331b1edb482c3de
Navdeep Saini <navdeepkataria111@gmail.com>
2024-08-21 20:22:19 +0530

    Code changes made to parse html file or content to get href link and text

---
+++ HtmlParser/example/ex1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	link "gophercises/htmlParser"
+	"strings"
+)
+
+var exampleHtml = `
+<html>
+<body>
+	<h1>Hello!</h1>
+	<a href="/other-page"> 
+		A link to another page 
+		<span> Just a span </span>
+	</a>
+	<a href="/second-page"> A link to second page </a>
+</body>
+</html>
+`
+
+func main() {
+	r := strings.NewReader(exampleHtml)
+
+	links, err := link.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", links)
+
+}
